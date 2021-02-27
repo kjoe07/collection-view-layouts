0531901e9d75fcf504b87c35bd3070d8b40a680a
Yoel Jimenez del Valle <yoel07@gmail.com>
2021-02-27 14:02:39 -0500

    add SPM support

---
+++ Tests/collection-view-layoutsTests/collection_view_layoutsTests.swift
@@ -0,0 +1,16 @@
+import UIKit
+import XCTest
+@testable import collection_view_layouts
+
+final class collection_view_layoutsTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+       // XCTAssertEqual(collection_view_layouts().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
